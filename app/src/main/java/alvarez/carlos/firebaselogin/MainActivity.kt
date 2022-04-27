e2a1d7fe2f726caabc1647d6d5e58d86dbdbfb36
Carlos Alberto Alvarez Rodriguez <carlos.alvarez216543@potros.itson.edu.mx>
2022-04-27 12:13:23 -0700

    Se agrego la funcion de registrar usuarios

--- app/src/main/java/alvarez/carlos/firebaselogin/MainActivity.kt
+++ app/src/main/java/alvarez/carlos/firebaselogin/MainActivity.kt
@@ -1,6 +1,7 @@
 package alvarez.carlos.firebaselogin
 
 import alvarez.carlos.firebaselogin.databinding.ActivityMainBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
@@ -19,5 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        signOut()
+    }
+
+    private fun signOut(){
+        Firebase.auth.signOut()
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 }
